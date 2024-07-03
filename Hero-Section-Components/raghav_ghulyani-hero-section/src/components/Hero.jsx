718308891c8dcbe9d5a7af3c75e094ad43896552
vahghar <raghav10g@gmail.com>
2024-07-03 11:00:58 +0530

    basic outlook for hero section

---
+++ Hero-Section-Components/raghav_ghulyani-hero-section/src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero container'>
+      <div className="hero-text">
+        <h1>we ensure better education for a better world</h1>
+        <p>it is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less.</p>
+        <button className='btn'>explore more</button>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ Hero-Section-Components/raghav_ghulyani-hero-section/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className='container'>
+        <img src="" alt="" />
+        <ul>
+            <li>Home</li>
+            <li>Program</li>
+            <li>About Us</li>
+            <li><button className='btn' >Contact Us</button></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
